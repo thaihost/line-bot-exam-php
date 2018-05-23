e4777dd32bd50becf3163fbd4419409ef67735ed
thaihost <mr.thaweesak@hotmail.com>
2018-05-24 00:20:35 +0700

    Update pushMsg.php

--- pushMsg.php
+++ pushMsg.php
@@ -1,8 +1,9 @@
 <?php
 require "vendor/autoload.php";
 require "config.php";
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->pushMessage('mr.casper', $textMessageBuilder);
