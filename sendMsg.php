3ee9f4269f747df1d44fbf002fb63268f7dd2a02
thaihost <mr.thaweesak@hotmail.com>
2018-05-23 11:20:47 +0700

    Update sendMsg.php

--- sendMsg.php
+++ sendMsg.php
@@ -4,7 +4,7 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('PdRhGi4R3ijgxA2BeKNqK
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '269d0c2229a8ecbc6b7e8844f721918d']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('mr.casper', $textMessageBuilder);
+$response = $bot->pushMessage('Ue79f91fc4c149e18249b25ccc65b0519', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
