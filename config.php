c10dcc2281aa19dde9395b495bc53c73bacd59e2
thaihost <mr.thaweesak@hotmail.com>
2018-05-23 14:17:00 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$access_token='PdRhGi4R3ijgxA2BeKNqK5l/UwCm986ywXM+PMX8jQMWYonPt6QFTfvxk5LkMmSUYpFOFu5sAOz8jL0dAbRNm8lGToB+JDyyQJaDqEM15VLcC7/hu7g6s7vJNWAfUys4H363hKe5lZQm9FoGXTQ8lQdB04t89/1O/w1cDnyilFU=';
+$chanel_secret='269d0c2229a8ecbc6b7e8844f721918d';
+?>
