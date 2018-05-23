47cf5db3e51d22b3b8c8f26bfb49d7436a34af91
thaihost <mr.thaweesak@hotmail.com>
2018-05-23 14:38:16 +0700

    Update getprofile.php

--- getprofile.php
+++ getprofile.php
@@ -1,5 +1,6 @@
 <?php
 include('config.php');
+echo $access_token;exit;
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
 $response = $bot->getProfile('<userId>');
