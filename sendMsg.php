8c0efd8a405b0cb6e03b31e42b6f2a58db9dc0fb
thaihost <mr.thaweesak@hotmail.com>
2018-05-23 17:15:37 +0700

    Update sendMsg.php

--- sendMsg.php
+++ sendMsg.php
@@ -1,5 +1,5 @@
 <?php
-require "vendor/autoload.php";
+//require "vendor/autoload.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('PdRhGi4R3ijgxA2BeKNqK5l/UwCm986ywXM+PMX8jQMWYonPt6QFTfvxk5LkMmSUYpFOFu5sAOz8jL0dAbRNm8lGToB+JDyyQJaDqEM15VLcC7/hu7g6s7vJNWAfUys4H363hKe5lZQm9FoGXTQ8lQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '269d0c2229a8ecbc6b7e8844f721918d']);
 
