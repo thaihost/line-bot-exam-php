a3baabe3c781947d203afc8aa795ecadd9158420
thaihost <mr.thaweesak@hotmail.com>
2018-05-24 00:13:29 +0700

    Update replyMsg.php

--- replyMsg.php
+++ replyMsg.php
@@ -6,6 +6,6 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('0f3779fba3b349968c5d07db31eab56f', $textMessageBuilder);
+$response = $bot->replyMessage('nHuyWiB7yP5Zw52FIkcQobQuGDXCTA', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
