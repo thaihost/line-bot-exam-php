61468ef8c32fd4e56a894c52bb6f31c00af4defb
thaihost <mr.thaweesak@hotmail.com>
2018-05-24 00:22:31 +0700

    Update pushMsg.php

--- pushMsg.php
+++ pushMsg.php
@@ -6,6 +6,6 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('mr.casper', $textMessageBuilder);
+$response = $bot->pushMessage('Ue79f91fc4c149e18249b25ccc65b0519', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
