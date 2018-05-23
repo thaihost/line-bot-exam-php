80a67a6fce37c3ec7a49a46fa87f97babca50f1c
thaihost <mr.thaweesak@hotmail.com>
2018-05-23 15:32:47 +0700

    Update getprofile.php

--- getprofile.php
+++ getprofile.php
@@ -1,7 +1,7 @@
 <?php
 include('config.php');
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => ''.$chanel_secret.'']);
 $response = $bot->getProfile('mr.casper');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
