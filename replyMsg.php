6a56cb30a4be0eedb8b5ac3e148f0c9e8ce5aa59
thaihost <mr.thaweesak@hotmail.com>
2018-05-24 00:01:42 +0700

    Update replyMsg.php

--- replyMsg.php
+++ replyMsg.php
@@ -1,10 +1,11 @@
 <?php
 require('vendor/autoload.php');
 require('config.php');
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+$response = $bot->replyMessage('nHuyWiB7yP5Zw52FIkcQobQuGDXCTA', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
