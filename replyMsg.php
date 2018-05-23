dba19d07c53ca8009b677671b70f5222ec262409
thaihost <mr.thaweesak@hotmail.com>
2018-05-23 23:59:00 +0700

    Create replyMsg.php

---
+++ replyMsg.php
@@ -0,0 +1,10 @@
+<?php
+require('vendor/autoload.php');
+require('config.php');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
