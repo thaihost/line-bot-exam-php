f56338b17be356a81aee273441ff3a0f06b304d0
thaihost <mr.thaweesak@hotmail.com>
2018-06-21 17:04:12 +0700

    Update sendMsg.php

--- sendMsg.php
+++ sendMsg.php
@@ -5,7 +5,7 @@ require "config.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
 
-$txt='สวัสดีครับทุกท่าน';
+$txt='สวัสดีครับทุกท่าน\nเป็นอย่างไรบ้าง';
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($txt);
 $response = $bot->pushMessage('Ue79f91fc4c149e18249b25ccc65b0519', $textMessageBuilder);
