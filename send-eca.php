a645b177e2775fdbb7ff35f664eb0285dc7daa2e
thaihost <mr.thaweesak@hotmail.com>
2018-08-14 01:30:41 +0700

    Update send-eca.php

--- send-eca.php
+++ send-eca.php
@@ -3,11 +3,12 @@ require "vendor/autoload.php";
 require "eca.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
-$txt='เรียน ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง
+$txt='เรียน เจ้าหน้าที่พัสดุ
 
-ตามที่กรมสรรพสามิตได้ลงนามทำสัญญาเลขที่ 5/2561 กับ ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง นั้ัน ในการส่งมอบงวดงานที่ 2 ได้มีการหักประกันผลงานไว้ เป็นจำนวน 50,000 บาท
+ตามที่กรมสรรพสามิตได้ลงนามทำสัญญาเลขที่ 5/2561 กับ ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง นั้ัน พบการชำรุดบกพร่องในวันที่ 10/10/2561 ของฝาผนังมีรอยร้าวขนาดกว้างและยาว
+ขอให้ทางบริษัทฯ เร่งเข้ามาดำเนินการปรับปรุงแก้ไขภายใน 7 วัน และดำเนินการให้ได้คุณภาพตามมาตรฐานสากล
 
-จึงแจ้งมาเพื่อทราบ';
+จึงแจ้งมาเพื่อทราบและโปรดดำเนินการต่อไป';
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($txt);
 $response = $bot->pushMessage('Ue96da58faf93ffecd17e309f43369a20', $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
