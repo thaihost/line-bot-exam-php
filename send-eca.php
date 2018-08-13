6f262ef083d08014e8ee951d62e7daf99d5e7e08
thaihost <mr.thaweesak@hotmail.com>
2018-08-14 01:32:53 +0700

    Update send-eca.php

--- send-eca.php
+++ send-eca.php
@@ -3,7 +3,7 @@ require "vendor/autoload.php";
 require "eca.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
-$txt='เรียน เจ้าหน้าที่พัสดุ
+$txt='เรียน ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง
 
 ตามที่กรมสรรพสามิตได้ลงนามทำสัญญาเลขที่ 5/2561 กับ ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง นั้ัน พบการชำรุดบกพร่องในวันที่ 10/10/2561 ของฝาผนังมีรอยร้าวขนาดกว้างและยาว
 ขอให้ทางบริษัทฯ เร่งเข้ามาดำเนินการปรับปรุงแก้ไขภายใน 7 วัน และดำเนินการให้ได้คุณภาพตามมาตรฐานสากล
