e05108174d47606675d02c9d7890419ca687d2fb
thaihost <mr.thaweesak@hotmail.com>
2018-08-14 01:02:39 +0700

    Update send-eca.php

--- send-eca.php
+++ send-eca.php
@@ -3,7 +3,7 @@ require "vendor/autoload.php";
 require "eca.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
-$txt='เรียน เจ้าหน้าที่พัสดุ
+$txt='เรียน ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง
 
 ตามที่กรมสรรพสามิตได้ลงนามทำสัญญาเลขที่ 5/2561 กับ ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง นั้ัน ในการส่งมอบงวดงานที่ 2 ได้มีการหักประกันผลงานไว้ เป็นจำนวน 50,000 บาท
 
