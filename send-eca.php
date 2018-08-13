4e4cd344642b7645ca9e4dc9b32fcb3e81c40afe
thaihost <mr.thaweesak@hotmail.com>
2018-08-14 01:00:35 +0700

    Update send-eca.php

--- send-eca.php
+++ send-eca.php
@@ -3,12 +3,11 @@ require "vendor/autoload.php";
 require "eca.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $chanel_secret]);
-$txt='เรียน ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง
+$txt='เรียน เจ้าหน้าที่พัสดุ
 
-ตามที่กรมสรรพสามิตได้ลงนามทำสัญญาเลขที่ 5/2561 กับ ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง นั้ัน 
-ขอให้มารับคืนหลักประกันสัญญาได้ ตั้งแต่วันที่ 02/03/2562 เป็นต้นไป
+ตามที่กรมสรรพสามิตได้ลงนามทำสัญญาเลขที่ 5/2561 กับ ห้างหุ้นส่วนจำกัด เอส พีบิลดิ้ง นั้ัน ในการส่งมอบงวดงานที่ 2 ได้มีการหักประกันผลงานไว้ เป็นจำนวน 50,000 บาท
 
-จึงแจ้งมาเพื่อทราบและโปรดดำเนินการต่อไป';
+จึงแจ้งมาเพื่อทราบ';
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($txt);
 $response = $bot->pushMessage('Ue96da58faf93ffecd17e309f43369a20', $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
